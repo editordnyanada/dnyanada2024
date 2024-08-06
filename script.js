cc514581239d43965ab99b9fce0f97b49c891b14
editordnyanada <editor_dnyanada@adcet.in>
2024-08-06 07:01:32 +0000

    update loop code

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
     const images = [
-        'https://github.com/editordnyanada/dnyanada2024/blob/f710017a2e6ca7c6fe04ff78fa81965e4ff95871/images/Dnayanda2024_Page_001.jpg?raw=true',
-        'https://raw.githubusercontent.com/editordnyanada/dnyanada2024/main/images/Dnayanda2024_Page_002.jpg'
-        // Add more image URLs
+        "https://github.com/editordnyanada/dnyanada2024/blob/f710017a2e6ca7c6fe04ff78fa81965e4ff95871/images/Dnayanda2024_Page_001.jpg?raw=true",
+        "https://raw.githubusercontent.com/editordnyanada/dnyanada2024/main/images/Dnayanda2024_Page_002.jpg"
     ];
 
     const flipbook = document.getElementById('flipbook');
 
     images.forEach((src, index) => {
-        const page = document.createElement('div');
-        page.className = 'page';
-        page.style.backgroundImage = `url(${src})`;
-        page.style.zIndex = images.length - index;
-        flipbook.appendChild(page);
+        const img = document.createElement('img');
+        img.src = src;
+        img.className = 'page';
+        img.alt = `Page ${index + 1}`;
+        flipbook.appendChild(img);
 
         page.addEventListener('click', () => {
             page.classList.toggle('flipped');
