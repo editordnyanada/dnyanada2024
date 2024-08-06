edfc3d7beca45dea2018b77829ed57e9d9aab500
editordnyanada <editor_dnyanada@adcet.in>
2024-08-06 08:48:54 +0000

    no flip

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
-// Array of image URLs
 const imageUrls = [
     'https://github.com/editordnyanada/dnyanada2024/blob/f710017a2e6ca7c6fe04ff78fa81965e4ff95871/images/Dnayanda2024_Page_001.jpg?raw=true',
     'https://github.com/editordnyanada/dnyanada2024/blob/f710017a2e6ca7c6fe04ff78fa81965e4ff95871/images/Dnayanda2024_Page_002.jpg?raw=true',
     'https://github.com/editordnyanada/dnyanada2024/blob/f710017a2e6ca7c6fe04ff78fa81965e4ff95871/images/Dnayanda2024_Page_003.jpg?raw=true',
     'https://github.com/editordnyanada/dnyanada2024/blob/f710017a2e6ca7c6fe04ff78fa81965e4ff95871/images/Dnayanda2024_Page_004.jpg?raw=true'
-    // Add more URLs as needed
 ];
 
 let currentPage = 0;
 
-const flipbookContainer = document.getElementById('flipbook-container');
 const bookImage = document.getElementById('bookImage');
 const prevButton = document.getElementById('prevButton');
 const nextButton = document.getElementById('nextButton');
@@ -17,13 +14,7 @@ const currentPageElement = document.getElementById('currentPage');
 const totalPagesElement = document.getElementById('totalPages');
 
 function updateImage() {
-    // Slide effect by changing the translateX value
-    flipbookContainer.style.transform = `translateX(-${currentPage * 100}%)`;
-
-    // Update the image source
     bookImage.src = imageUrls[currentPage];
-    
-    // Update page indicators
     currentPageElement.textContent = currentPage + 1; // Display page numbers as 1-based
     totalPagesElement.textContent = imageUrls.length;
 }
@@ -32,4 +23,15 @@ prevButton.addEventListener('click', () => {
     if (currentPage > 0) {
         currentPage--;
         updateImage();
-   
+    }
+});
+
+nextButton.addEventListener('click', () => {
+    if (currentPage < imageUrls.length - 1) {
+        currentPage++;
+        updateImage();
+    }
+});
+
+// Initialize with the first page
+updateImage();
