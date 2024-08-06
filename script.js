06dc9ba6cd2fa489cb923da81dd1840d9e553b8c
editordnyanada <editor_dnyanada@adcet.in>
2024-08-06 06:52:43 +0000

    trying different link

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const images = [
-        'https://raw.githubusercontent.com/editordnyanada/dnyanada2024/main/images/Dnayanda2024_Page_001.jpg',
+        'https://github.com/editordnyanada/dnyanada2024/blob/f710017a2e6ca7c6fe04ff78fa81965e4ff95871/images/Dnayanda2024_Page_001.jpg?raw=true',
         'https://raw.githubusercontent.com/editordnyanada/dnyanada2024/main/images/Dnayanda2024_Page_002.jpg'
         // Add more image URLs
     ];
